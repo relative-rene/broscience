3f9ddf47a694af98713c6056567f5c07702d74c3
relative-rene <rene.arellano415@gmail.com>
2017-07-12 13:52:25 -0700

    save react and bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,11 @@
 //
 //= require rails-ujs
 //= require turbolinks
+//= require react
+//= require react_ujs
+//= require components
 //= require_tree .
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+// PRODUCTION REMINDER require the concatenated bootstrap for faster compilation:
